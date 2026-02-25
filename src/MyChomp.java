9e3e56f991087059ec8cda0e7c908c902af5bd5b
astranono <90936000+astranono@users.noreply.github.com>
2026-02-25 11:25:19 -0500

    did board

---
+++ src/MyChomp.java
@@ -0,0 +1,34 @@
+public class MyChomp {
+    int i = 3;
+    int k = 3;
+    int j = 3;
+    int boardnum = 0;
+    String output = "";
+    String temp = "";
+
+    //todo:print out all the 3x3 boards
+    public String createboard(){
+
+        for (i=3; i > -1; i--){
+            if (i == 0){
+                break;
+            }
+            for (j=i; j > -1; j--) {
+                for (k=j; k > -1; k--) {
+                    boardnum +=1;
+                    System.out.println("board: " + i + ", " + j + ", " + k);
+                    System.out.println("board count: " + boardnum);
+                }
+            }
+        }
+        return (temp);
+    }
+
+
+    public static void main(String[] args) {
+        MyChomp game = new MyChomp();
+        game.createboard();
+    }
+}
+
+
