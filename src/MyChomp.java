3f144f7d5f13585735656d5c672fd74f08376beb
astranono <90936000+astranono@users.noreply.github.com>
2026-04-30 21:44:11 -0400

    done

--- src/MyChomp.java
+++ src/MyChomp.java
@@ -1,168 +1,108 @@
+import java.util.*;
 import java.io.*;
-import java.util.Arrays;
-import java.util.HashMap;
 
 public class MyChomp implements Serializable {
-    //alter to 10, save board then done
-    //slightly increases preformance
-    //https://stackoverflow.com/questions/14274480/static-final-long-serialversionuid-1l
-    private static final long serialVersionUID = 1L;
-    int size = 4;
-    private static class StateInfo implements Serializable {
-        private static final long serialVersionUID = 1L;
-        String result; // "W" for winning "L" for lose
-        String bestMove;
-
-        StateInfo(String result, String bestMove) {
-            this.result = result;
-            this.bestMove = bestMove;
-        }
-    }
-
-    public HashMap<String, StateInfo> states = new HashMap<>();
-    public void createboard() {
-        int[] startHeights = new int[size];
-        //https://www.w3schools.com/java/ref_arrays_fill.asp
-        Arrays.fill(startHeights, size);
-    }
-
+    private static final long serialVersionUID = 2L;
+    int size = 10; // Set to 10 for the larger board
+    //initially, I used a hashmap that stored the entire board as a string with stateinfo, however this was incredibly unoptimized
+    public HashMap<Long, Byte> states = new HashMap<>(200000);
+    //200000 was chosen because the capacity of a 10x10 is somewhere around 184k states
+    //I looked it up and resizing is incredibly unoptimal, and setting the entries to a hard cap prevents constly resizing while attempting to compute the states
     public boolean boardsolver(int[] heights) {
-        String state = Arrays.toString(heights);
+        //bytes are also used bc I am storing very few bits and using a long would be extra wasted memory, this saves a ton.(great wombo)
+        //my old method attempted to solve for around 25 billion possibilities, which explained why it took so long when
+        long packed = pack(heights);
 
-        // Check if the state is already solved
-        if (states.containsKey(state)) {
-            return states.get(state).result.equals("W");
-        }
+        Byte info = states.get(packed);
+        //winstate flag, look up how to get the mask up but it masks
+        if (info != null) return (info & 0x80) != 0;
 
-        // Special case: losing state (only one chip left)
-        if (heights[0] == 1 && (heights.length == 1 || heights[1] == 0)) {
-            states.put(state, new StateInfo("L", null));
+        // exception, bad last square case so you have to make it false
+        if (heights[0] == 1 && (size == 1 || heights[1] == 0)) {
+            //https://www.w3schools.com/java/ref_hashmap_put.asp
+            states.put(packed, (byte) 0);
             return false;
         }
-        //add comment, maybe one on game theory
-        // Try all possible moves
-        for (int c = 0; c < size; c++) { // Columns
-            for (int r = 0; r < heights[c]; r++) { // Rows
-                if (r == 0 && c == 0) {
-                    continue; // Skip the last square
-                }
+
+        for (int c = 0; c < size; c++) {
+            for (int r = 0; r < heights[c]; r++) {
+                if (r == 0 && c == 0) continue;
 
                 int[] nextState = simulateChomp(heights, r, c);
-                if (!boardsolver(nextState)) { // Recursive step
-                    states.put(state, new StateInfo("W", r + "," + c)); // Winning move
+                if (!boardsolver(nextState)) {
+                    states.put(packed, encode(r, c));
                     return true;
                 }
             }
         }
 
-        // If no winning move is found, mark as losing state
-        states.put(state, new StateInfo("L", null));
+        states.put(packed, (byte) 0);
         return false;
     }
 
-    public String getBestMove(int[] heights) {
-        String state = Arrays.toString(heights);
-        StateInfo info = states.get(state);
-        return (info != null && info.bestMove != null) ? info.bestMove : "No move available";
+// the reason I use bit packing and longs is because it is SIGNIFICANTLY faster than the strings I did before
+    //it is also more complex however the time savings offset that. I store every game state as a long bit
+    public long pack(int[] heights) {
+        long res = 0;
+        for (int i = 0; i < size; i++) {
+            //what this does is do a left shift operator, so that the res can fit a clmumn  into it, and then it index through it to keep doing it and "pack it"
+            res = (res << 4) | (heights[i]);
+        }
+        return res;
     }
-
+    //https://stackoverflow.com/questions/42071459/bitwise-pack-multiple-numbers-into-a-byte
+     byte encode(int r, int c) {
+        //reason or is used is because it is faster than arithematic.
+         //0x80 flag is used to force first bit to be one which is winning state in the byte that returned, connection to above thing
+         //row is shifted 3 times to get out of c way, both take up 3 binary digits,
+        return (byte) (0x80 | (r << 3) | c);
+    }
+    //https://stackoverflow.com/questions/42071459/bitwise-pack-multiple-numbers-into-a-byte
     public int[] simulateChomp(int[] heights, int row, int col) {
-        int[] newHeights = Arrays.copyOf(heights, heights.length);
-
-        // Update the heights for all columns >= col
-        for (int c = col; c < size; c++) {
-            newHeights[c] = Math.min(newHeights[c], row);
+        int[] next = heights.clone();
+        for (int i = col; i < size; i++) {
+            if (next[i] > row) next[i] = row;
         }
-
-        return newHeights;
+        return next;
     }
 
-    public int[] convertToHeights(Chip[][] board) {
-        int[] heights = new int[size];
 
-        // Calculate the height of each column
-        for (int c = 0; c < size; c++) {
-            int height = 0;
-            for (int r = 0; r < board.length; r++) {
-                if (board[r][c] != null) { // assime null means no chip
-                    height++;
-                } else {
-                    break;
-                }
-            }
-            heights[c] = height;
-        }
-
-        return heights;
-    }
-    //needed for saving the states tothe seralize file
     public void saveStates(String filename) {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
             oos.writeObject(this.states);
-            System.out.println("saved to: " + filename);
+            System.out.println("Saved " + states.size() + " states to " + filename);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    //file loader
-    //https://stackoverflow.com/questions/1129795/what-is-suppresswarnings-unchecked-in-java
+
     @SuppressWarnings("unchecked")
     public void loadStates(String filename) {
         File file = new File(filename);
-        if (!file.exists()) return;
-
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
-            this.states = (HashMap<String, StateInfo>) ois.readObject();
-            System.out.println("States loaded successfully from " + filename);
+            this.states = (HashMap<Long, Byte>) ois.readObject();
+            System.out.println("Loaded " + states.size());
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public void logAllBestMoves() {
-        // Generate all possible board states
-        generateAndSolveBoards(new int[size], size - 1);
-
-        // Log all states and their best moves
-        // : form works best bc other methods would take sigificnatly more lines. 
-        for (String state : states.keySet()) {
-            StateInfo info = states.get(state);
-            System.out.println("State: " + state + ", Result: " + info.result + ", Best Move: " + info.bestMove);
-        }
-    }
-
-    private void generateAndSolveBoards(int[] heights, int column) {
-        if (column < 0) {
-            boardsolver(heights);
-            return;
-        }
-
-        for (int h = 0; h <= size; h++) {
-            heights[column] = h;
-            generateAndSolveBoards(heights, column - 1);
-        }
-    }
 
     public static void main(String[] args) {
         MyChomp game = new MyChomp();
-        String saveFile = "chomp_states.dat";
-        //without this, it will try to trwrite to chomp_states every single time you load
-        // Try to load existing data
-        game.loadStates(saveFile);
+        String saveFile = "chomp_optimized.dat";
 
-        if (game.states.isEmpty()) {
-            System.out.println("No save");
-            long startTime = System.nanoTime();
-            game.logAllBestMoves();
-            long endTime = System.nanoTime();
-
-            // Save after calculation
-            game.saveStates(saveFile);
-
-            double durationMs = (endTime - startTime) / 1_000_000.0;
-            System.out.println("Calculation Time: " + durationMs + " ms");
+        // try to load existing data
+        File file = new File(saveFile);
+        if (file.exists()) {
+            System.out.println("Loading: " + saveFile);
+            game.loadStates(saveFile);
+            System.out.println("Current state c: " + game.states.size());
         } else {
-            System.out.println("Loaded " + game.states.size() + " states");
+            System.out.println("nsv, fix");
         }
+        long start = System.currentTimeMillis();
+        game.saveStates(saveFile);
+        System.out.println(" Time: " + (System.currentTimeMillis() - start) + "ms");
     }
 }
--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -1,37 +1,70 @@
 import java.awt.*;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class MyPlayer {
     public Chip[][] gameBoard;
-    public int[] columns;
+    private MyChomp solver;
+    private final String SAVE_FILE = "chomp_optimized.dat";
 
     public MyPlayer() {
-        columns = new int[10];
-
-        /***
-         * This code will run just once, when the game opens.
-         * Add your code here.
-         */
+        solver = new MyChomp();
+        solver.loadStates(SAVE_FILE);
     }
 
     public Point move(Chip[][] pBoard) {
+        this.gameBoard = pBoard;
 
-        System.out.println("MyPlayer Move");
+        int[] heights = convertToHeights(pBoard);
 
-        gameBoard = pBoard;
-        int column = 0;
-        int row = 0;
+        long packedState = solver.pack(heights);
 
-        row = 1;
-        column = 1;
+        Byte info = solver.states.get(packedState);
 
-        /***
-         * This code will run each time the "MyPlayer" button is pressed.
-         * Add your code to return the row and the column of the chip you want to take.
-         * You'll be returning a data type called Point which consists of two integers.
-         */
+        if (info != null && (info & 0x80) != 0) {
+            int r = (info >> 3) & 0x7;
+            int c = info & 0x7;
 
-        Point myMove = new Point(row, column);
-        return myMove;
+            System.out.println("AI Move Found! Playing: Row " + r + ", Col " + c);
+
+            // In your Chomp.java: row = theMove.getX(), col = theMove.getY()
+            // So we put Row in X and Col in Y
+            return new Point(r, c);
+        }
+
+        System.out.println("no move found");
+        return playsafe(heights);
     }
 
+    private int[] convertToHeights(Chip[][] board) {
+        int[] heights = new int[10];
+
+        // loop through  columns
+        for (int c = 0; c < 10; c++) {
+            int h = 0;
+            for (int r = 0; r < 10; r++) {
+                //checks isalive and null
+                if (board[r][c] != null && board[r][c].isAlive) {
+                    h++;
+                } else {
+                    break;
+                }
+            }
+            heights[c] = h;
+        }
+
+        System.out.println("Solver sees heights: " + Arrays.toString(heights));
+        return heights;
+    }
+//for some reason sometimes it misses so this ensure that if it cna't find a move, it will attempt to alter the board to find a winning wombo.
+    private Point playsafe(int[] heights) {
+        for (int c = 9; c >= 0; c--) {
+            if (heights[c] > 0) {
+                int r = heights[c] - 1;
+                if (r == 0 && c == 0) continue;
+                return new Point(r, c);
+            }
+        }
+        return new Point(0, 0);
+    }
 }
